f528334591ff24e5c71c131c99026d6abc4436dd
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2026-03-03 14:57:17 +0000

    initial commit

---
+++ src/main/java/com/group5/gw1/Gw1Application.java
@@ -0,0 +1,13 @@
+package com.group5.gw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/group5/gw1/Gw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.gw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
