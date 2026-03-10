02700d26a4bb9a3d8afaf2f8d0c3b5980782d68f
Syed Fasiuddin <sf403@student.le.ac.uk>
2026-03-10 12:59:32 +0000

    add interface for resource ranking service

---
+++ src/main/java/com/group5/gw1/service/ResourceRankingService.java
@@ -0,0 +1,11 @@
+package com.group5.gw1.service;
+
+import com.group5.gw1.dto.RankedResourceResult;
+import com.group5.gw1.dto.ResourceRequest;
+
+import java.util.List;
+
+public interface ResourceRankingService {
+    // TODO-6: create package similar to TODO-3 and implement this
+    List<RankedResourceResult> getRankedResources(ResourceRequest request);
+}
