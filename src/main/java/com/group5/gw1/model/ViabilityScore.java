b940df80fb393c20169fc9a7322e38d481bedae2
ls627-uol <ls627@student.le.ac.uk>
2026-03-16 15:41:11 +0000

    feat: add ViabilityScore class with score attributes and getters

--- src/main/java/com/group5/gw1/model/ViabilityScore.java
+++ src/main/java/com/group5/gw1/model/ViabilityScore.java
@@ -1,8 +1,30 @@
 package com.group5.gw1.model;
 
 public class ViabilityScore {
+    public ViabilityScore(double total, double proximity, double accessibility, double weatherSafety) {
+        this.totalScore = total;
+        this.proximityScore = proximity;
+        this.accessibilityScore = accessibility;
+        this.weatherSafetyScore = weatherSafety;
+    }
     private double totalScore;
     private double proximityScore;
     private double accessibilityScore;
     private double weatherSafetyScore;
+
+    public double getTotalScore() {
+        return totalScore;
+    }
+
+    public double getProximityScore() {
+        return proximityScore;
+    }
+
+    public double getAccessibilityScore() {
+        return accessibilityScore;
+    }
+
+    public double getWeatherSafetyScore() {
+        return weatherSafetyScore;
+    }
 }
